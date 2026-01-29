f7f32c936319496d81041a0624050e777611a144
luanbianchini <luanbianchine1@gmail.com>
2026-01-29 15:17:27 -0300

    feat: clean ball when fall in ground after 2 seconds

--- game/entities/projectile.py
+++ game/entities/projectile.py
@@ -1,3 +1,4 @@
+import time
 from ..physics.adapter.physics_adapter import PhysicsProjectileAdapter
 from constants.environment import SCREEN_WIDTH
 
@@ -5,6 +6,7 @@ class Projectile:
     def __init__(self, pos):
         self.pos_x = pos[0]
         self.pos_y = pos[1]
+        self.landed_time = None
 
 class ProjectileFactory:
     def __init__(self, physics):
@@ -31,3 +33,12 @@ class ProjectileFactory:
             if adapter.body.position.x > SCREEN_WIDTH or adapter.body.position.x < 0:
                 self.physics.remove(adapter)
                 self.projectiles.remove((projectile, adapter))
+                continue
+            if abs(adapter.body.position.y) <= 550:
+                if projectile.landed_time is None:
+                    projectile.landed_time = time.time()
+                elif time.time() - projectile.landed_time >= 2:
+                    self.physics.remove(adapter)
+                    self.projectiles.remove((projectile, adapter))
+            else:
+                projectile.landed_time = None
